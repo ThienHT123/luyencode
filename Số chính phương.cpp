95eaf29992ec5f06ad20024f2f1b9f0cd35a907b
ThienHT <87663094+ThienHT123@users.noreply.github.com>
2022-06-25 20:06:17 +0700

    SCP
    code c++

---
+++ Số chính phương.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int n;
+    for(int i=0;i<t;++i){
+        cin>>n;
+        cout<<int(sqrt(n))<<endl;
+	}
+}
